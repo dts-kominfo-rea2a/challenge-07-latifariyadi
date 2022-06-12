3726da02139723c7593780b894f3ea4219a5a8a9
Latif Ariyadi <latifariyadi86@gmail.com>
2022-06-12 19:07:32 +0700

    first commit

--- promise.js
+++ promise.js
@@ -1,8 +1,56 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async(emosi) => {
+    const arrIXX = await promiseTheaterIXX();
+    const arrVGC = await promiseTheaterVGC();
+
+    try {
+        const perhitunganPertamaVGC = await penjumlahanEmosi(0, emosi, arrVGC[0]);
+        const perhitunganKeduaVGC = await penjumlahanEmosi(
+            perhitunganPertamaVGC,
+            emosi,
+            arrVGC[1]
+        );
+        const totalVGC = await penjumlahanEmosi(
+            perhitunganKeduaVGC,
+            emosi,
+            arrVGC[2]
+        );
+
+        const perhitunganPertamaIXX = await penjumlahanEmosi(0, emosi, arrIXX[0]);
+        const perhitunganKeduaIXX = await penjumlahanEmosi(
+            perhitunganPertamaIXX,
+            emosi,
+            arrIXX[1]
+        );
+        const totalIXX = await penjumlahanEmosi(
+            perhitunganKeduaIXX,
+            emosi,
+            arrIXX[2]
+        );
+
+        return totalIXX + totalVGC;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const penjumlahanEmosi = (nilaiAwal, emosi, arrBioskop) => {
+    return new Promise((resolve, reject) => {
+        if (arrBioskop.hasil !== null) {
+            if (arrBioskop.hasil === emosi) {
+                return resolve(nilaiAwal + 1);
+            } else {
+                return resolve(nilaiAwal);
+            }
+        } else {
+            reject(`Data Error`);
+        }
+    });
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
